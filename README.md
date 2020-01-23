30e2aa4c1099ae916ef03d29a5752762c1bf04c8
stefanoyoyo <git.stefano@outlook.it>
2020-01-23 11:05:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dhgpsz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dhgpsz)
---
+++ src/app/ste-component/ste-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ste-component',
+  templateUrl: './ste-component.component.html',
+  styleUrls: ['./ste-component.component.css']
+})
+export class SteComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
